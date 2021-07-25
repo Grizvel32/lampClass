48f3e4b61c6293411a8a9db580911a2dcc662fc7
Grizvel32 <grizenkov1996@gmail.com>
2021-07-25 10:17:23 +0300

    first commit

---
+++ src/com/company/Lamp.java
@@ -0,0 +1,109 @@
+package com.company;
+
+public class Lamp {
+    // region Enum Color
+    public enum Type{
+        led("светодиодная"),
+        glow("накаливания"),
+        halogen("галогеновая");
+
+        private final String value;
+
+        Type(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+    public enum Color {
+        red("красный"),
+        yellow("желтый"),
+        white("белый");
+        private final String value;
+
+        Color(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+    //endregion
+
+    //region Variables
+    private int power;
+    private int colourTemperature;
+    private String base;
+    private String brandName;
+    private Type type;
+    private Color color;
+
+    //endregion
+
+    //region Constructor
+
+    public Lamp() {
+        power = 12;
+        colourTemperature = 4000;
+        base = "E27";
+        brandName = "ecola";
+        type = Type.led;
+        color = Color.white;
+    }
+
+
+    public Lamp(int power, int colourTemperature, String base, String brandName, Type type, Color color) {
+        this.power = power;
+        this.colourTemperature = colourTemperature;
+        this.base = base;
+        this.brandName = brandName;
+        this.type = type;
+        this.color = color;
+    }
+
+    public Lamp(Lamp lamp) {
+        this.power = power;
+        this.colourTemperature = colourTemperature;
+        this.base = base;
+        this.brandName = brandName;
+        this.type = type;
+        this.color = color;
+    }
+    //endregion
+
+    //region Setters
+
+    public void setPower(int power) throws Exception {
+
+        if (power <= 0) {
+            throw new Exception("engine power must be           grate than 0");
+        }
+        this.power = power;
+
+    }
+    //endregion
+
+    //region Getters
+
+    public int getPower(){
+        return power;
+    }
+    //endregion
+
+    public String getInString() {
+        String output = "";
+
+        output += "мощность лампы " + power + "(Вт)\n";
+        output += "цветовая температура " + colourTemperature + "(К)\n";
+        output += "цоколь " + base + "\n";
+        output += "марка " + brandName + "\n";
+        output += "тип лампы " + type.getValue() + "\n";
+        output += "цвет " + color.getValue() + "\n";
+
+        return output;
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int powerLamp, colourTemperature, base, brandName,type;
+
+        int action = -1;
+        boolean isRun = true;
+
+        while (isRun) {
+
+            System.out.println("Меню:");
+            System.out.println("1. Создать стандартную лампочку ");
+            System.out.println("2. Создать лампочку с заданными параметрами");
+
+            System.out.println("0. Выход");
+
+            System.out.print("Введите номер пункта меню: ");
+            action = input.nextInt();
+
+            switch (action) {
+                case 1: {
+                    Lamp lamp1 = new Lamp();
+                    System.out.println(lamp1.getInString());
+                }
+                break;
+                case 2: {
+                    Lamp lamp2 = new Lamp(8,4000,"E27","philips",Lamp.Type.halogen,Lamp.Color.yellow);
+                    System.out.println(lamp2.getInString());
+                }
+                case 0: {
+                    isRun = false;
+                }
+                break;
+
+                default: {
+                    System.out.println("Ошибка. Такой команды не существует");
+                }
+                break;
+            }
+            }
+        }
+    }
